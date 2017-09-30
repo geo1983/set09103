a0a485acadc19b1ac06fe8676a5599d58366f9bc
geo1983 <catalin_21_geo@yahoo.com>
2017-09-30 13:10:26 +0000

    URL variables

--- hello.py
+++ hello.py
@@ -1,27 +1,10 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-  return "The default 'root' route"
+@app.route("/hello/<name>")
+def hello(name):
+  return "hello %s" % name
 
-@app.route("/account/", methods=['GET', 'POST'])
-def account():
-  if request.method == 'POST':
-    print request.form
-    name = request.form['name']
-    return "hello %s" % name
-  else:
-    page ='''
-    <html><body>
-      <form action="" method="post" name="form">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name"/>
-        <input type="submit" name="submit" id="submit"/>
-      </form>
-      </body></html>'''
-    return page
-      
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
