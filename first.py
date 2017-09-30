e6c5169348842068e3aed2004b1a0d4ab66f0040
geo1983 <catalin_21_geo@yahoo.com>
2017-09-30 10:35:35 +0000

    hello george

--- first.py
+++ first.py
@@ -1,10 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  return "Hi George"
-  
-if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+print "hello George"
 
