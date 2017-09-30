0588aa398f5a1a561031e5d4d62699933ed5fef5
geo1983 <catalin_21_geo@yahoo.com>
2017-09-30 13:15:49 +0000

    URL int and floats varibles

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/hello/<name>")
-def hello(name):
-  return "hello %s" % name
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+  return str(first+second)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
