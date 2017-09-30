9af80547f64025fe356c389ce11928af5c028d39
geo1983 <catalin_21_geo@yahoo.com>
2017-09-30 11:08:19 +0000

    the default 404

--- hello.py
+++ hello.py
@@ -1,15 +1,13 @@
-from flask import Flask, redirect, url_for
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/private")
-def private():
-  #  Test for user logged failed
-  #  so redirect to login URL
-  return redirect(url_for('login'))
+@app.route("/")
+def hello():
+  return "Hello George"
 
-@app.route('/login')
-def login():
-  return "Now we would get username $ pass"
+@app.errorhandler(404)
+def page_not_found(error):
+  return "Couldn't find the page you requested", 404
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
