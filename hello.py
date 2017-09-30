a53cc4ff5060eb4ef4d756993d1c410ed1b94da9
geo1983 <catalin_21_geo@yahoo.com>
2017-09-30 11:47:41 +0000

    static files

--- hello.py
+++ hello.py
@@ -1,17 +1,16 @@
-from flask import Flask, abort
+from flask import Flask, url_for, abort
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello George"
 
-@app.route('/force404')
-def force404():
-  abort(404)
-
-@app.errorhandler(404)
-def page_not_found(error):
-  return "Couldn't find the page you requested", 404
+@app.route('/static-example/img')
+def static_example_img():
+  start = '<img src="'
+  url = url_for('static', filename='vmask.jpg')
+  end = '">'
+  return start+url+end, 200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
