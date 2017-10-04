8f6d4ef5ced315e67fdcf550869ec74184aa26e5
geo1983 <catalin_21_geo@yahoo.com>
2017-10-04 13:34:16 +0000

    display uploaded image

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
 app = Flask(__name__)
+
+@app.route("/display/")
+def display():
+  return '<img src="'+url_for('static', filename='uploads/upload.png')+'"/>'
 
 @app.route("/account/", methods=['POST','GET'])
 def account():
