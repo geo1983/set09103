272cb5fc44371ca029e940741748455079ce53de
geo1983 <catalin_21_geo@yahoo.com>
2017-10-04 12:42:34 +0000

    URL parameters

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/add/<int:first>/<int:second>")
-def add(first, second):
-  return str(first+second)
+@app.route("/hello/")
+def hello():
+  name = request.args.get('name', '')
+  if name == '':
+    return "no param supplied"
+  else:
+    return "hello %s" % name
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
