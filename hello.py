4aeb0bfdf42e0830ac036af00cf42adfa6981fbc
geo1983 <catalin_21_geo@yahoo.com>
2017-10-05 09:58:57 +0000

    templates and collections

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-  return render_template('conditional.html', name=name)
+@app.route('/users/')
+def users():
+  names = ['george', 'jay', 'monica', 'delia', 'edy']
+  return render_template('loops.html', names=names)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
